e47082f58a68f62fa08832b698e972c510dc359e
sarbjeet <sarbjeets18@gmail.com>
2021-04-03 00:43:46 +0100

    Register Page is created

---
+++ app/src/main/java/uk/ac/tees/S0321466/registerUser.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.S0321466;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registerUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+    }
+}
